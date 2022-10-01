76e50e1fe756643c7c311db881110222750d7982
DANIELE ENGOME <57971424+Daniele19@users.noreply.github.com>
2022-10-01 07:33:23 -0700

    Create skills.js

---
+++ models/skills.js
@@ -0,0 +1,43 @@
+const skills = [
+  {skill: 'HTML', level: '3', description: 'I have adequate proficiently using this skill in a variety of applications.'},
+  {skill: 'CSS', level: '3', description: 'I have adequate proficiently using this skill in a variety of applications.'},
+  {skill: 'CSS Bootstrap', level: '1', description: 'I have limited experience with this skill at this point.'},
+  {skill: 'JavaScript', level: '3', description: 'I have adequate proficiently using this skill in a variety of applications.'},
+  {skill: 'MongoDB', level: '1', description: 'I have limited experience with this skill at this point.'},
+  {skill: 'Mongoose', level: '2', description: 'I have moderate experience with this skill.'},
+  {skill: 'Express.js', level: '2', description: 'I have moderate experience with this skill.'},
+  {skill: 'React', level: '0', description: 'I have not begun my studies of this skill yet.'},
+  {skill: 'Node.js', level: '1', description: 'I have limited experience with this skill at this point.'},
+  {skill: 'Python', level: '0', description: 'I have not begun my studies of this skill yet.'},
+  {skill: 'Django', level: '0', description: 'I have not begun my studies of this skill yet.'},
+  {skill: 'PostgreSQL', level: '0', description: 'I have not begun my studies of this skill yet.'},
+];
+
+module.exports = {
+  getAll,
+  getOne,
+  create, 
+  deleteOne,
+  update
+};
+
+function update(id, skill) {
+  skills[id] = skill;
+}
+
+function deleteOne(id) {
+  skills.splice(id, 1);
+}
+
+function create(skill) {
+  skills.push(skill);
+}
+
+function getOne(id){
+  return skills[id];
+}
+
+function getAll() {
+  return skills;
+}
+Footer
